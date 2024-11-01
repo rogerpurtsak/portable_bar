4931c90020c7104265bc53bf1fce7a8317cfaa55
Kristin <krvare@taltech.ee>
2024-11-01 11:47:16 +0200

    I made a reservation form, I added css and input text

--- frontend/src/pages/Broneeri.jsx
+++ frontend/src/pages/Broneeri.jsx
@@ -3,8 +3,14 @@ import "./broneeri.css";
 function Broneeri() {
     return (
       <div className="broneeri-body">
-        <h2 className="broneeri-text">Broneeri yap</h2>
+        <h2 className="broneeri-text">Broneeri</h2>
         <p>Muu kirjeldav tekst!</p>
+        <form className="broneeri-form"></form>
+        <input type="text" placeholder="Ees- ja perekonnanimi" />
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Joogivalikust loe rohkem" />
+        <button type="submit">Broneeri</button>
+
       </div>
     );
   }
