fda2e28a6fb12e93cf6085c690b0abe889d422e2
ropurt <ropurt@taltech.ee>
2024-11-11 23:58:29 +0200

    Footer, navbar and Avaleht responsive

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,18 +1,27 @@
-import { Link } from "react-router-dom"; // Et kasutada eri failides <Link> tööriista, peate selle iga kord importima
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [isLinksVisible, setIsLinksVisible] = useState(false);
+
+  const toggleLinks = () => {
+    setIsLinksVisible(!isLinksVisible);
+  };
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${isLinksVisible ? "expanded" : ""}`}>
       <Link to="/">
         <img className="logo-img" src="hood_logo.png" alt="HOOD logo" />
       </Link>
-      {/* Linki sees to="" väärtus peab klappima App.jsx failis oleva Route path="" sisuga. */}
-      <div className="links">
-      <Link to="/" className="nav-link">Avaleht</Link>
-      <Link to="/meist" className="nav-link">Meist</Link>
-      <Link to="/galerii" className="nav-link">Galerii</Link>
-      <Link to="/broneeri" className="nav-link">Broneeri</Link>
-      <Link to="/login" className="nav-link">Profiil</Link>
+      <button className="menu-button" onClick={toggleLinks}>
+        Menu
+      </button>
+      <div className={`links ${isLinksVisible ? "show" : ""}`}>
+        <Link to="/" className="nav-link">Avaleht</Link>
+        <Link to="/meist" className="nav-link">Meist</Link>
+        <Link to="/galerii" className="nav-link">Galerii</Link>
+        <Link to="/broneeri" className="nav-link">Broneeri</Link>
+        <Link to="/login" className="nav-link">Profiil</Link>
       </div>
     </div>
   );
